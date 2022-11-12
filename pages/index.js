cc82ad07eb7610a216e127bfbf6b205535a3621e
chandrakant-karole <chandrakantkarole001995@gmail.com>
2022-11-12 11:12:21 +0530

    fixed error

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          It's Chandrakant Karole
+          Its Chandrakant Karole
         </h1>
 
         <p className={styles.description}>
